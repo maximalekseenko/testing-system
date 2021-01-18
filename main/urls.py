06858c4ab8680190126485b3115eae7307b95d1d
Ivan Afonin <heresybepurged31m@gmail.com>
2021-01-18 23:55:36 +0300

    added static; minor fixes)

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url, include
-from main.views import HomeView
+from main.views import HomeView, HeresyoneView
 
 urlpatterns = [
+    url('heresyone/', HeresyoneView),
     url('', HomeView),
 ]
--- main/views.py
+++ main/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def HomeView(request):
     return render(request, 'home.html')
+
+def HeresyoneView(request):
+    return  render(request, 'heresypage1.html')
