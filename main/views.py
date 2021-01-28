5c17985ff1ec59ba0bd80d90f7c6144b2570545b
MaksKhan <handusmaksim@gmail.com>
2021-01-28 23:28:47 +0300

    almost home page

--- main/views.py
+++ main/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 
 def HomeView(request):
-    return render(request, 'home.html')
+    is_teacher = True
+    args={"is_teacher": is_teacher}
+    return render(request, 'home.html', args)
 
 def HeresyoneView(request):
     return  render(request, 'heresypage1.html')
