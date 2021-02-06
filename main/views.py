6792fa54fbcb5cc82206d03612995f9c89b2adc5
Ivan Afonin <heresybepurged31m@gmail.com>
2021-02-06 18:05:58 +0300

    user profile fixes (W)

--- main/views.py
+++ main/views.py
@@ -1,17 +1,14 @@
 from django.contrib.auth.models import User
 from django.shortcuts import render
-from .models import test1
 
 def HomeView(request):
-    user_data = User.objects.get(username=request.user).UserProfile
-    args={"user_data": user_data}
+    user = request.user
+    args={"user": user}
     return render(request, 'home.html', args)
 
 def HeresyoneView(request):
-    test = test1.objects.first()
 
     args={
-        "test" : test,
         "TEST" : "TEST",
     }
 
