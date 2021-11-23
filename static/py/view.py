4f30bb8ea52d36600b574c04796442f3317db4bd
maximalekseenko <max.alekseenko@gmail.com>
2021-11-23 16:37:49 +0300

    view touched

--- static/py/view.py
+++ static/py/view.py
@@ -12,6 +12,9 @@ def get_base_context(request, pagename, buttonname=""):
 
 
     if request.user.is_authenticated:
+        # context['group-module_dic'] = {group:module Group.objects.filter(author__id=request.user.id)}
+        # context['modules_groups'] = {group:set(Module.objects.filter(is_active=True, assigned_to=group)) for group in Group.objects.filter(members__id=request.user.id)}
+
         context['my_modules']     = Module.objects.filter(author=request.user)
         context['my_groups']      = Group.objects.filter(author__id=request.user.id)
         context['modules_groups'] = {group:set(Module.objects.filter(is_active=True, assigned_to=group)) for group in Group.objects.filter(members__id=request.user.id)}
