a7ad8b77e42db461fda151a1b97c2aeaac58dcb6
maximalekseenko <max.alekseenko@gmail.com>
2022-03-03 10:12:07 +0300

    deploy fix2

--- testing_system/settings.py
+++ testing_system/settings.py
@@ -79,8 +79,8 @@ USE_TZ = True
 
 # Static files
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
-
+# STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
 # login urls
 LOGIN_URL = 'login/'
